5af0d0b27b892809f36e86ed03cbd5d115a95783
ivo <ivo@MacBook-Pro-ivo.local>
2023-01-14 21:13:01 +0300

    feat:tailwind

--- jsn-vue/cryptonomicon/src/App.vue
+++ jsn-vue/cryptonomicon/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+  <div class="container mx-auto flex flex-col items-center  p-4">
     <!-- <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
       <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
         viewBox="0 0 24 24">
@@ -218,4 +218,3 @@ export default {
 }
 </script>
 
-<style src="./app.css"></style>
