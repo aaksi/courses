e9c3affa1db673fbb33d1d3c4a62421d1614ee9b
aaksi <kulikovstanislav55@gmail.com>
2023-01-28 19:09:58 +0300

    feat: add input-range-vue

---
+++ input-range-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <inputRange></inputRange>
+</template>
+
+<script>
+import inputRange from "./components/inputRange.vue"
+
+export default {
+  name: "App",
+  components: {
+    inputRange,
+  },
+}
+</script>
+
+<style></style>
