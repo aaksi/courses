94833686d06fe3caa9e31ba885844a6519eb2cdc
ivo <ivo@MacBook-Pro-ivo.local>
2023-01-30 00:32:23 +0300

    fix: input-range-vue

--- vue/input-range-vue/src/components/inputRange.vue
+++ vue/input-range-vue/src/components/inputRange.vue
@@ -8,8 +8,8 @@
     />
     <input
       type="range"
-      :min=" minCarat "
-      :max=" maxCarat "
+      :min="minCarat"
+      :max="maxCarat"
       step="0.01"
       v-model="rangeValue"
     />
@@ -25,24 +25,41 @@ export default {
       rangeValue: 0.03,
       minCarat: 0.03,
       maxCarat: 2.99,
+      ratio: 0.2,
     }
   },
   computed: {
     calcValue() {
-      return (this.rangeValue * 0.2).toFixed(3)
+      const gram = String(this.rangeValue * this.ratio)
+      if (gram.length > 4) {
+        console.log(gram.length)
+        console.log(gram)
+        return Number(gram).toFixed(3)
+      } else {
+        return gram
+      }
     },
   },
   methods: {
     gramCalc(event) {
+      // if (event.target.value.length > 4) {
+      //   console.log(event.target.value.length)
+      //   return event.target.value.length.toFixed(2)
+      // }
+
+      // if(event.target.value.length > 4){
+      //   event.target.value = Number(event.target.value).toFixed(3)
+      // }
+
       if (event.target.value * 5 > this.maxCarat) {
         this.rangeValue = this.maxCarat
       } else if (event.target.value * 5 < this.minCarat) {
         this.rangeValue = this.minCarat
+        // event.target.value = this.minCarat * 0.2
       } else {
         this.rangeValue = (event.target.value * 5).toFixed(2)
         // let inputVal = Number(event.target.value)
         // event.target.value = inputVal.toFixed(2)
-
       }
     },
   },
