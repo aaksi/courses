27829a538604ff434d01f7fe3bd15d08d26673ec
aaksi <kulikovstanislav55@gmail.com>
2023-01-31 22:52:37 +0300

    actual

--- vue/input-range-vue/src/components/inputRange.vue
+++ vue/input-range-vue/src/components/inputRange.vue
@@ -1,52 +1,47 @@
 <template>
-  <div>
+  <div class="wrap">
     <input
       class="input input--min"
       type="number"
       @input="gramCalc"
       v-model="calcValue"
     />
-    <template>
-      <vue-slider v-model="value" />
-    </template>
-    <input
-      class="range"
-      type="range"
-      :min="minCarat"
-      :max="maxCarat"
-      step="0.01"
-      v-model="rangeValue"
-    />
+    <div class="vue-slider-wrap">
+      <vue-slider
+        v-model="rangeValue"
+        :min="minCarat"
+        :max="maxCarat"
+        :interval="step"
+      />
+    </div>
     <input type="number" v-model="rangeValue" />
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue"
 import VueSlider from "vue-slider-component"
-import "vue-slider-component/theme/antd.css"
+import "vue-slider-component/theme/default.css"
 
 export default {
   name: "inputRange",
 
-  setup() {
-    const data = reactive({ value: 0 })
-    return toRefs(data)
+  components: {
+    VueSlider,
   },
+
   data() {
     return {
       rangeValue: 0.03,
       minCarat: 0.03,
       maxCarat: 2.99,
       ratio: 0.2,
+      step: 0.01
     }
   },
   computed: {
     calcValue() {
       const gram = String(this.rangeValue * this.ratio)
       if (gram.length > 4) {
-        console.log(gram.length)
-        console.log(gram)
         return Number(gram).toFixed(3)
       } else {
         return gram
@@ -55,24 +50,13 @@ export default {
   },
   methods: {
     gramCalc(event) {
-      // if (event.target.value.length > 4) {
-      //   console.log(event.target.value.length)
-      //   return event.target.value.length.toFixed(2)
-      // }
-
-      // if(event.target.value.length > 4){
-      //   event.target.value = Number(event.target.value).toFixed(3)
-      // }
 
       if (event.target.value * 5 > this.maxCarat) {
         this.rangeValue = this.maxCarat
       } else if (event.target.value * 5 < this.minCarat) {
         this.rangeValue = this.minCarat
-        // event.target.value = this.minCarat * 0.2
       } else {
         this.rangeValue = (event.target.value * 5).toFixed(2)
-        // let inputVal = Number(event.target.value)
-        // event.target.value = inputVal.toFixed(2)
       }
     },
   },
@@ -86,5 +70,16 @@ export default {
   width: 380px;
   border-radius: 10px;
   padding: 0 65px 0 45px;
+}
+.vue-slider {
+  width: 100px;
+}
+.wrap {
+  display: flex;
+  gap: 30px;
+}
+.vue-slider-wrap {
+  display: block;
+  width: 100px;
 }
 </style>
