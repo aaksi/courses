8f435ec3b7974bc7c7e2718bb9c55071d9a4836d
aaksi <kulikovstanislav55@gmail.com>
2025-02-19 14:36:20 +0300

    actual

--- vue/vue-aviasales-clone/src/components/HeroSection.vue
+++ vue/vue-aviasales-clone/src/components/HeroSection.vue
@@ -1,10 +1,14 @@
-<script setup></script>
+<script setup>
+const pageTitle = 'Тут покупают дешёвые авиабилеты'
+</script>
 
 <template>
   <div class="c-hero-section">
     <div class="c-hero-section__wrap">
       <div class="container">
-        <div class="c-hero-section__inner"></div>
+        <div class="c-hero-section__inner">
+          <h1>{{ pageTitle }}</h1>
+        </div>
       </div>
     </div>
   </div>
@@ -17,5 +21,6 @@
 }
 .c-hero-section__inner {
   min-height: 80vh;
+  padding: 100px 0;
 }
 </style>
